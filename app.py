0a51a94dad896570dd59bb0b4961ccdbfff38233
MohammadAhmed108 <122654720+MohammadAhmed108@users.noreply.github.com>
2023-11-27 00:40:58 -0800

    Flask backend

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify, render_template
+import tensorflow as tf
+import numpy as np
+import keras
+
+app = Flask(__name__)
+model = keras.models.load_model('SA_Trained_Model.keras')  # Load your model
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/analyze', methods=['POST'])
+def analyze_text():
+    data = request.get_json(force=True)
+    input_text = data['text']
+    prediction = model.predict(np.array([input_text]))  # Use the model to predict
+    return jsonify({'prediction': float(prediction[0])})
+
+if __name__ == '__main__':
+    app.run(debug=True)
