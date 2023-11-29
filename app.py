9e706130dc5bbbc272b74c4ad023125a51adaf8f
MohammadAhmed108 <122654720+MohammadAhmed108@users.noreply.github.com>
2023-11-28 20:12:52 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,32 @@
 from flask import Flask, request, jsonify, render_template
 import tensorflow as tf
 import numpy as np
-import keras
 
 app = Flask(__name__)
-model = keras.models.load_model('OPT_LSTM_model.keras')  # Load  model
+
+# Exception handling for model loading
+try:
+    model = tf.keras.models.load_model('C:/Users/Affan/OneDrive - Rebel Hosting/Desktop/171 group project/OPT_LSTM_model.keras.url')  # Ensure this path is correct
+    print("Model loaded successfully.")
+except Exception as e:
+    print(f"Error loading the model: {e}")
 
 @app.route('/')
 def home():
+    # Ensure that 'templates/index.html' exists
     return render_template('index.html')
 
 @app.route('/analyze', methods=['POST'])
 def analyze_text():
-    data = request.get_json(force=True)
-    input_text = data['text']
-    prediction = model.predict(np.array([input_text]))  # Use the model to predict
-    return jsonify({'prediction': float(prediction[0])})
+    try:
+        data = request.get_json(force=True)
+        input_text = data['text']
+        # Preprocess input_text here if necessary
+        prediction = model.predict(np.array([input_text]))  # Use the model to predict
+        # Ensure prediction is in the correct format to be sent as JSON
+        return jsonify({'prediction': float(prediction[0])})
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
