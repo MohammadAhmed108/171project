4fe7f22971c29782cf24512bd3aa67a7313b3314
MohammadAhmed108 <122654720+MohammadAhmed108@users.noreply.github.com>
2023-12-08 23:48:52 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,31 @@
 from flask import Flask, request, jsonify, render_template
 import tensorflow as tf
+from keras.preprocessing.text import tokenizer_from_json
+from keras.preprocessing.sequence import pad_sequences
+import keras
 import numpy as np
 import os
 import logging
+import json
 
 app = Flask(__name__)
 
 # Setup logging
 logging.basicConfig(level=logging.INFO)
 
-# Confirm the current working directory and the existence of the model file
-model_path = 'OPT_LSTM_model1.keras'  # Update this if your model is in a different path
-app.logger.info(f"Current working directory: {os.getcwd()}")
-if os.path.exists(model_path):
-    app.logger.info(f"Found model file at {model_path}")
-else:
-    app.logger.error(f"Model file not found at {model_path}")
+# Parameters
+total_vocabulary = 5000
+max_len = 100
 
-# Load model with error handling
+# Load the tokenizer
+with open('vocabulary_tokenizer.json', 'r') as f:
+    tokenizer_config = f.read()
+tokenizer = keras.preprocessing.text.tokenizer_from_json(tokenizer_config)
+
+# Load the model
+model_path = 'SA_Trained_Model.h5'
 try:
-    model = tf.keras.models.load_model(model_path)
+    model = keras.models.load_model(model_path)
     app.logger.info("Model loaded successfully.")
 except Exception as e:
     app.logger.error(f"Error loading the model: {e}")
@@ -28,12 +34,15 @@ except Exception as e:
 def home():
     return render_template('index.html')
 
-@app.route('/analyze', methods=['POST'])
+@app.route('/predict', methods=['POST'])  # Changed from '/analyze' to '/predict'
 def analyze_text():
     try:
         data = request.get_json(force=True)
         input_text = data['text']
-        prediction = model.predict(np.array([input_text]))  # Adjust for model's expected input format
+        app.logger.info(f"Received text: {input_text}")
+        input_seq = pad_sequences(tokenizer.texts_to_sequences([input_text]), padding='post', maxlen=max_len)
+        prediction = model.predict(input_seq)
+        app.logger.info(f"Prediction: {prediction}")
         return jsonify({'prediction': float(prediction[0])})
     except Exception as e:
         app.logger.error(f"Error during prediction: {e}")
